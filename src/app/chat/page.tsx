7d2425fce00bece3d5f4cef1215f3c1fbdfc081d
ENTEI <entei@MacBook-Pro-Entei.local>
2025-10-03 20:57:37 +0200

    add lunbi

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,11 @@
+export default function Chat() {
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="flex-1">
+        <div className="flex flex-col h-full items-center justify-center bg-gray-100">
+          <div className="flex-1 w-full max-w-md">Lunbi</div>
+        </div>
+      </div>
+    </div>
+  );
+}
